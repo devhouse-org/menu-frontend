8933441a1eab4f5ca6ade3d301d45f65a1dbbf45
Nada-235 <nada.alshakiry@gmail.com>
2024-05-22 12:07:45 +0300

    Header Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import logo from './assets/logo.jpg'
+
 function App() {
   return (
-    <>
-      <div className="w-screen h-screen flex justify-center items-center bg-pink">
-      Hello World
+    <> 
+      <div className="w-screen flex flex-col items-center p-4">
+        <div className='w-full flex justify-center'>
+          <img src={logo} alt='Logo' className='w-28'/>
+        </div>
+
+        <h1 className='pt-3'>We hope your meal was as delightful as you hoped!</h1>
+        <h1 className='text-red-400'>نأمل أن تكون وجبتك كانت ممتعة كما تمنيت</h1>
+
       </div>
     </>
   )
