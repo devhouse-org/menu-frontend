ca10ab65057e69879f63bfbdc3bfd6a91fb23771
Ahmed Ghazi <ahmed.ghazi@iqdevhouse.com>
2024-05-28 15:14:13 +0300

    adding simple sidebar with functionality

--- src/Pages/MenuPage.tsx
+++ src/Pages/MenuPage.tsx
@@ -6,13 +6,15 @@ import Modal from "../components/Menu/Modal";
 const MenuPage: React.FC = () => {
 	// State to track which modal is open
 	const [openModalId, setOpenModalId] = useState<
-		number | null
+		string | null
 	>(null);
+	const [cat, setCat] = useState(0)
 
 	// Handle opening a modal
-	const handleOpenModal = (itemId: number) => {
+	const handleOpenModal = (itemId: string) => {
 		setOpenModalId(itemId);
 	};
+
 
 	//Handle closing a modal
 	const handleCloseModal = () => {
@@ -31,46 +33,62 @@ const MenuPage: React.FC = () => {
 				}}
 			></div>
 
-			<h1 className='pt-24 text-white z-10 text-6xl font-medium'>
-				Menu
+			<h1 className='pt-24 text-white z-10 text-4xl font-medium'>
+				{menuItems[cat].category}
 			</h1>
+			<div className="flex gap-3">
+				{/* side bar */}
+				<div className="w-3/12  z-10 bg-white rounded-lg">
+					<h2 className="p-3  font-bold ">Food Categories</h2>
+					<ul className="mt-5">
+						{
+							menuItems.map((item, index) => {
+								return <li className={`p-2  cursor-pointer ${index === cat ? 'bg-blue-500 text-white' : 'hover:bg-gray-100'}`} onClick={() => setCat(index)} key={item.category}>{item.category}</li>
+							})
+						}
+					</ul>
+				</div>
+				{/* Items Card */}
+				<div className='w-9/12 grid md:grid-cols-3 grid-cols-2 gap-4 z-10 py-16'>
+					{menuItems[cat].foods.map((item) => (
+						<button
+							key={item.name}
+							className='border rounded-3xl p-4 bg-white/90 backdrop-blur-lg flex flex-col gap-4'
+						onClick={() => handleOpenModal(item.name)}
+						>
+							<div className='w-full flex justify-center items-center'>
+								<img
+									src={item.image}
+									alt={item.name}
+									className='w-full object-contain'
+								/>
+							</div>
 
-			{/* Items Card */}
-			<div className='grid md:grid-cols-3 grid-cols-2 gap-4 z-10 py-16'>
-				{menuItems.map((item) => (
-					<button
-						key={item.id}
-						className='border rounded-3xl p-4 bg-white/90 backdrop-blur-lg flex flex-col gap-4'
-						onClick={() => handleOpenModal(item.id)}
-					>
-						<div className='w-full flex justify-center items-center'>
-							<img
-								src={item.image}
-								alt={item.name}
-								className='w-full object-contain'
-							/>
-						</div>
+							<div className='flex flex-col justify-center gap-2'>
+								<h2 className='w-full text-start font-bold text-lg'>
+									{item.name}
+								</h2>
+								<p className='text-coral-600 text-md font-bold flex items-end w-full'>
+									<span className='text-Yale-Blue-900 pr-1'>
+										Price:
+									</span>
+									{item.price}
+								</p>
+							</div>
+						</button>
+					))}
+				</div>
 
-						<div className='flex flex-col justify-center gap-2'>
-							<h2 className='w-full text-start font-bold text-2xl'>
-								{item.name}
-							</h2>
-							<p className='text-coral-600 text-xl font-bold flex items-end w-full'>
-								<span className='text-Yale-Blue-900 pr-1'>
-									Price:
-								</span>
-								{item.price}
-							</p>
-						</div>
-					</button>
-				))}
 			</div>
 
+
 			{/* Modals */}
-			{menuItems.map((item) => (
+
+
+			{menuItems[cat].foods.map((item) => (
 				<Modal
-					key={item.id}
-					open={openModalId === item.id}
+					key={item.name}
+					open={openModalId === item.name}
 					onClose={handleCloseModal}
 				>
 					{/* Modal content for each item */}
