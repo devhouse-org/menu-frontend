b9353b8cb8ac8934faad2cf0247d91de61e34c71
Nada-235 <nada.alshakiry@gmail.com>
2024-05-29 11:31:14 +0300

    unfiniahed UI

--- src/Pages/MenuPage.tsx
+++ src/Pages/MenuPage.tsx
@@ -6,11 +6,12 @@ import Modal from "../components/Menu/Modal";
 const MenuPage: React.FC = () => {
 	// State to track which modal is open
 	const [openModalId, setOpenModalId] = useState<
-		number | null
+		string | null
 	>(null);
+	const [cat, setCat] = useState(0);
 
 	// Handle opening a modal
-	const handleOpenModal = (itemId: number) => {
+	const handleOpenModal = (itemId: string) => {
 		setOpenModalId(itemId);
 	};
 
@@ -20,8 +21,8 @@ const MenuPage: React.FC = () => {
 	};
 
 	return (
-		<div className='relative w-screen min-h-screen flex flex-col justify-center items-center font-montserrat text-Yale-Blue-900 p-6'>
-			{/* Background Pattern */}
+		<div className='relative w-screen min-h-screen flex flex-col  items-center font-montserrat text-Yale-Blue-900 p-6'>
+			{/* Background Pattern
 			<div
 				className='absolute bg-Yale-Blue-900 w-full h-full z-0 backdrop-blur-sm'
 				style={{
@@ -29,52 +30,80 @@ const MenuPage: React.FC = () => {
 					backgroundSize: "cover",
 					backgroundPosition: "center",
 				}}
-			></div>
+			></div> */}
 
-			<h1 className='pt-24 text-white z-10 text-6xl font-medium'>
-				Menu
-			</h1>
+			<div className=' pt-24 flex flex-col items-center bg-green-400'>
+				<h1 className=' text-white z-10 text-4xl font-medium bg-pink-400 w-full'>
+					{menuItems[cat].category}
+				</h1>
+				<div className='flex w-full gap-3 bg-blue-500'>
+					{/* sidebar */}
+					<div className='w-3/12 z-10 bg-white/9 0 rounded-lg'>
+						<h2 className='font-bold w-full py-5 flex justify-center items-center'>
+							Food Categories
+						</h2>
+						<ul className='mt-5 bg-yellow-500'>
+							{menuItems.map((item, index) => {
+								return (
+									<li
+										className={`p-2 cursor-pointer ${
+											index === cat
+												? "bg-blue-500 text-white"
+												: "hover:bg-gray-100"
+										}`}
+										onClick={() => setCat(index)}
+										key={item.category}
+									>
+										{item.category}
+									</li>
+								);
+							})}
+						</ul>
+					</div>
 
-			{/* Items Card */}
-			<div className='grid md:grid-cols-3 grid-cols-2 gap-4 z-10 py-16'>
-				{menuItems.map((item) => (
-					<button
-						key={item.id}
-						className='border rounded-3xl p-4 bg-white/90 backdrop-blur-lg flex flex-col gap-4'
-						onClick={() => handleOpenModal(item.id)}
-					>
-						<div className='w-full flex justify-center items-center'>
-							<img
-								src={item.image}
-								alt={item.name}
-								className='w-full object-contain'
-							/>
-						</div>
+					{/* Items Card */}
+					<div className='w-9/12 grid md:grid-cols-3 grid-cols-2 gap-4 z-10 py-16 bg-purple-400'>
+						{menuItems[cat].foods.map((item) => (
+							<button
+								key={item.name}
+								className='border w-full rounded-3xl p-4 bg-white/90 backdrop-blur-lg flex flex-col gap-4'
+								onClick={() => handleOpenModal(item.name)}
+							>
+								<div className='w-full flex justify-center items-center'>
+									<img
+										src={item.image}
+										alt={item.name}
+										className='w-full object-contain'
+									/>
+								</div>
 
-						<div className='flex flex-col justify-center gap-2'>
-							<h2 className='w-full text-start font-bold text-2xl'>
-								{item.name}
-							</h2>
-							<p className='text-coral-600 text-xl font-bold flex items-end w-full'>
-								<span className='text-Yale-Blue-900 pr-1'>
-									Price:
-								</span>
-								{item.price}
-							</p>
-						</div>
-					</button>
-				))}
+								<div className='flex flex-col justify-center gap-2'>
+									<h2 className='w-full text-start font-bold text-lg'>
+										{item.name}
+									</h2>
+									<p className='text-coral-600 text-md font-bold flex items-end w-full'>
+										<span className='text-Yale-Blue-900 pr-1'>
+											Price:
+										</span>
+										{item.price}
+									</p>
+								</div>
+							</button>
+						))}
+					</div>
+				</div>
 			</div>
 
 			{/* Modals */}
-			{menuItems.map((item) => (
+
+			{menuItems[cat].foods.map((item) => (
 				<Modal
-					key={item.id}
-					open={openModalId === item.id}
+					key={item.name}
+					open={openModalId === item.name}
 					onClose={handleCloseModal}
 				>
 					{/* Modal content for each item */}
-					<div className="flex flex-col gap-2" >
+					<div>
 						<img
 							src={item.image}
 							alt={item.name}
