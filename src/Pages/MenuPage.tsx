0c4a350408d48d77af2f5866747b9efa715cf186
Yaser Asaad <yaser.asaad@iqdevhouse.com>
2024-08-15 15:19:12 +0300

    general fixes on frontend

--- src/Pages/MenuPage.tsx
+++ src/Pages/MenuPage.tsx
@@ -6,246 +6,276 @@ import { useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { getThemeColors } from "../utils";
 import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
-import { NumericFormat } from 'react-number-format';
 
 const MenuPage = () => {
-	// State to track which modal is open
-	const [openModalId, setOpenModalId] = useState<
-		string | null
-	>(null);
-	const [showSideBar, setShowSideBar] = useState(true)
-	const [cat, setCat] = useState(0);
-	const { t } = useTranslation();
-	const theme = getThemeColors();
+  // State to track which modal is open
+  const [openModalId, setOpenModalId] = useState<string | null>(null);
+  const [showSideBar, setShowSideBar] = useState(true);
+  const [cat, setCat] = useState(0);
+  const { t, i18n } = useTranslation();
+  const theme = getThemeColors();
 
-	const { data, isError, isPending } = useQuery({
-		queryKey: ["menu"],
-		queryFn: async () => {
-			const response = await axiosInstance.get(
-				`/restaurant/access/${localStorage.getItem(
-					"accessCode"
-				)}`
-			);
-			return response.data;
-		},
-	});
+  const { data, isError, isPending } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const response = await axiosInstance.get(
+        `/restaurant/access/${localStorage.getItem("accessCode")}`
+      );
+      return response.data;
+    },
+  });
 
-	// console.log(
-	// 	"theme",
-	// 	JSON.parse(localStorage.getItem("theme"))
-	// );
+  // console.log(
+  // 	"theme",
+  // 	JSON.parse(localStorage.getItem("theme"))
+  // );
 
-	// Handle opening a modal
-	const handleOpenModal = (itemId: string) => {
-		setOpenModalId(itemId);
-	};
+  // Handle opening a modal
+  const handleOpenModal = (itemId: string) => {
+    setOpenModalId(itemId);
+  };
 
-	// Handle closing a modal
-	const handleCloseModal = () => {
-		setOpenModalId(null);
-	};
-	// Add the effect to set sidebar state to false on mobile
-	useEffect(() => {
-		const handleResize = () => {
-			if (window.innerWidth <= 768) {
-				setShowSideBar(false);
-			}
-		};
+  // Handle closing a modal
+  const handleCloseModal = () => {
+    setOpenModalId(null);
+  };
+  // Add the effect to set sidebar state to false on mobile
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setShowSideBar(false);
+      }
+    };
 
-		// Initial check
-		handleResize();
+    // Initial check
+    handleResize();
 
-		// Add event listener for resize
-		window.addEventListener('resize', handleResize);
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
 
-		// Cleanup event listener on component unmount
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
-	if (isPending) {
-		return <div>Loading...</div>;
-	}
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
 
-	if (isError) {
-		return <div>Error</div>;
-	}
+  if (isError) {
+    return <div>Error</div>;
+  }
 
-	return (
-		<div
-			className='relative min-h-screen min-w-screen flex font-montserrat'
-			style={{
-				color: theme.primary,
-				backgroundColor: "white",
-			}}
-		>
-			<div className='pt-24'>
-				<div className='flex p-4 gap-2 relative'>
-					{/* sidebar */}
-					<div
-						className={`${showSideBar ? 'md:w-3/12' : ' w-0'} transition-all max-h-[600px] font-semibold sticky top-28 mt-10 h-fit overflow-y-auto`}
-						style={{
-							color: theme.primary,
-							backgroundColor: "white",
-						}}
-					>
-						<h2 className='font-bold w-full py-5 flex justify-center items-center'>
-							{t("Food Categories")}
-						</h2>
-						<hr
-							className='w-fill h-1 border-0 rounded '
-							style={{
-								backgroundColor: theme.secondary,
-							}}
-						></hr>
+  return (
+    <div
+      className="relative min-h-screen min-w-screen flex font-montserrat overflow-auto"
+      style={{
+        color: theme.primary,
+        backgroundColor: "white",
+      }}
+    >
+      <div className="pt-24">
+        <div className="flex p-4 gap-2 relative">
+          {/* sidebar */}
+          <div
+            className={`${
+              showSideBar ? "md:w-[50%]" : " w-0"
+            } transition-all max-h-[600px] font-semibold sticky top-28 mt-10 h-fit overflow-y-auto`}
+            style={{
+              color: theme.primary,
+              backgroundColor: "white",
+            }}
+          >
+            <h2 className="font-bold w-full py-5 flex justify-center items-center">
+              {t("Food Categories")}
+            </h2>
+            <hr
+              className="w-fill h-1 border-0 rounded "
+              style={{
+                backgroundColor: theme.secondary,
+              }}
+            ></hr>
 
-						<ul className='mt-5 text-sm md:text-lg'>
-							{data.categories.map(
-								(item: any, index: number) => {
-									return (
-										<li
-											className={`p-2 pl-3 cursor-pointer transition-all ${index === cat ? "rounded-xl" : ""
-												}`}
-											style={{
-												backgroundColor:
-													index === cat
-														? theme.secondary
-														: "transparent",
-												color:
-													index === cat
-														? "white"
-														: theme.primary,
-											}}
-											onClick={() => setCat(index)}
-											key={item.name}
-										>
-											{t(item.name)}
-										</li>
-									);
-								}
-							)}
-						</ul>
-					</div>
+            <ul className="mt-5 text-sm md:text-lg">
+              {data.categories.map((item: any, index: number) => {
+                return (
+                  <li
+                    className={`p-2 pl-3 cursor-pointer transition-all ${
+                      index === cat ? "rounded-xl" : ""
+                    }`}
+                    style={{
+                      backgroundColor:
+                        index === cat ? theme.secondary : "transparent",
+                      color: index === cat ? "white" : theme.primary,
+                    }}
+                    onClick={() => setCat(index)}
+                    key={item.name}
+                  >
+                    {t(item.name)}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
 
-					<div className={`${showSideBar ? 'md:w-9/12 ' : 'w-full'} `}>
+          <div
+            className={`${showSideBar ? "md:w-[50%] lg:w-[70%] " : "w-full"} `}
+          >
+            <div className="">
+              <div className="flex items-center">
+                {i18n.language === "en" ? (
+                  showSideBar ? (
+                    <ArrowBigLeft
+                      size={35}
+                      onClick={() => {
+                        setShowSideBar(false);
+                      }}
+                      className="cursor-pointer hover:bg-gray-100"
+                    />
+                  ) : (
+                    <ArrowBigRight
+                      size={35}
+                      onClick={() => {
+                        setShowSideBar(true);
+                      }}
+                      className="cursor-pointer hover:bg-gray-100"
+                    />
+                  )
+                ) : showSideBar ? (
+                  <ArrowBigRight
+                    size={35}
+                    onClick={() => {
+                      setShowSideBar(false);
+                    }}
+                    className="cursor-pointer hover:bg-gray-100"
+                  />
+                ) : (
+                  <ArrowBigLeft
+                    size={35}
+                    onClick={() => {
+                      setShowSideBar(true);
+                    }}
+                    className="cursor-pointer hover:bg-gray-100"
+                  />
+                )}
 
-						<div className=''>
-							<div className="flex items-center">
-								{
-									showSideBar ? 
-									<ArrowBigLeft size={35} onClick={() => { setShowSideBar(false) }} className="cursor-pointer hover:bg-gray-100 " />
-									:
-									<ArrowBigRight size={35} onClick={() => { setShowSideBar(true) }} className="cursor-pointer hover:bg-gray-100 " />
-								}
-								<h1
-									className='z-10 text-5xl font-bold text-center w-full'
-									style={{ color: theme.primary }}
-								>
-									{t(data?.categories[cat]?.name)}
-								</h1>
+                <h1
+                  className="z-10 text-5xl font-bold text-center w-full"
+                  style={{
+                    color: theme.primary,
+                    textAlign: i18n.language === "en" ? "left" : "right",
+                  }}
+                >
+                  {t(data?.categories[cat]?.name)}
+                </h1>
 
-								{/* Items Card */}
-							</div>
-							<div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 z-10 py-16">
-								{data?.categories[cat]?.items.map(
-									(item: any) => (
-										<div
-											key={item.name}
-											className='border w-full rounded-lg p-3 shadow-md hover:shadow-lg flex flex-col gap-4'
-											style={{
-												backgroundColor: "white",
-											}}
-											onClick={() =>
-												handleOpenModal(item.name)
-											}
-										>
-											<div className='w-full flex justify-center items-center'>
-												<img
-													src={
-														item.image
-															? item.image
-															: "https://luigispizzakenosha.com/wp-content/uploads/placeholder.png"
-													}
-													alt={item.name}
-													className='lg:h-48 lg:w-full h-28 w-80 object-cover rounded-xl'
-												/>
-											</div>
+                {/* Items Card */}
+              </div>
 
-											<div className='flex flex-col justify-center gap-2 w-full'>
-												<h2 className='w-full text-start font-bold text-sm md:text-lg truncate'>
-													{item.name}
-												</h2>
-												<p
-													className='text-sm md:text-md font-bold flex items-end w-full'
-													style={{
-														color: theme.secondary,
-													}}
-												>
-													<span
-														className='pr-1'
-														style={{
-															color: theme.primary,
-														}}
-													>
-														Price:
-													</span>
-													<span className="mx-1">IQD</span>
-													 <NumericFormat value={item.price} thousandSeparator=","/>
-												</p>
-											</div>
-										</div>
-									)
-								)}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+              {/* Items Card */}
 
-			{/* Modals */}
-			{data?.categories[cat]?.items.map((item: any) => (
-				<Modal
-					key={item.name}
-					open={openModalId === item.name}
-					onClose={handleCloseModal}
-				>
-					{/* Modal content for each item */}
-					<div>
-						<img
-							src={
-								item.image
-									? item.image
-									: "https://luigispizzakenosha.com/wp-content/uploads/placeholder.png"
-							}
-							alt={item.name}
-							className='w-full object-contain rounded-2xl'
-						/>
-						<h2 className='w-full text-center font-bold h-1/4 text-2xl py-3'>
-							{item.name}
-						</h2>
-						<p>{item.description}</p>
-						<p
-							className='text-sm md:text-md font-bold flex items-end w-full'
-							style={{
-								color: theme.secondary,
-							}}
-						>
-							<span
-								className='pr-1'
-								style={{
-									color: theme.primary,
-								}}
-							>
-								Price:
-							</span>
-							<span className="mx-1">IQD</span>
-							 <NumericFormat value={item.price} thousandSeparator=","/>
-						</p>
-					</div>
-				</Modal>
-			))}
-		</div>
-	);
+              <div
+                className={`grid ${
+                  showSideBar
+                    ? "lg:grid-cols-2 "
+                    : "lg:grid-cols-3 grid-cols-2 "
+                }  gap-4 z-10 py-16 px-6 `}
+              >
+                {data?.categories[cat]?.items.map((item: any) => (
+                  <div
+                    key={item.name}
+                    className="border w-full rounded-lg p-3 shadow-md hover:shadow-lg flex flex-col gap-4 "
+                    style={{
+                      backgroundColor: "white",
+                    }}
+                    onClick={() => handleOpenModal(item.name)}
+                  >
+                    <div className="w-full flex justify-center items-center">
+                      <img
+                        src={
+                          item.image
+                            ? item.image
+                            : "https://luigispizzakenosha.com/wp-content/uploads/placeholder.png"
+                        }
+                        alt={item.name}
+                        className="lg:h-48 lg:w-full h-28 w-80 object-cover rounded-xl"
+                      />
+                    </div>
+
+                    <div className="flex flex-col justify-center gap-2 w-full">
+                      <h2 className="w-full text-start font-bold text-sm md:text-lg truncate">
+                        {t(item.name)}
+                      </h2>
+                      <p
+                        className="text-sm md:text-md font-bold flex items-end w-full"
+                        style={{
+                          color: theme.secondary,
+                        }}
+                      >
+                        <span
+                          className="pr-1"
+                          style={{
+                            color: theme.primary,
+                          }}
+                        >
+                          Price:
+                        </span>
+                        <span className="mx-1">{t("IQD")}</span>
+                        <div>{item.price.toLocaleString()}</div>
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Modals */}
+      {data?.categories[cat]?.items.map((item: any) => (
+        <Modal
+          key={item.name}
+          open={openModalId === item.name}
+          onClose={handleCloseModal}
+        >
+          {/* Modal content for each item */}
+          <div>
+            <img
+              src={
+                item.image
+                  ? item.image
+                  : "https://luigispizzakenosha.com/wp-content/uploads/placeholder.png"
+              }
+              alt={item.name}
+              className="w-full object-contain rounded-2xl"
+            />
+            <h2 className="w-full text-center font-bold h-1/4 text-2xl py-3">
+              {t(item.name)}
+            </h2>
+            <p>{item.description}</p>
+            <p
+              className="text-sm md:text-md font-bold flex items-end w-full"
+              style={{
+                color: theme.secondary,
+              }}
+            >
+              <span
+                className="pr-1"
+                style={{
+                  color: theme.primary,
+                }}
+              >
+                {t("Price:")}
+              </span>
+              <span className="mx-1">{t("IQD")}</span>
+              <div>{item.price.toLocaleString()}</div>
+            </p>
+          </div>
+        </Modal>
+      ))}
+    </div>
+  );
 };
 
 export default MenuPage;
--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -3,32 +3,27 @@ import { useTranslation } from "react-i18next";
 import { getThemeColors } from "../utils";
 
 function LanguageSelector() {
-	const { i18n } = useTranslation();
-	const theme = getThemeColors();
+  const { i18n } = useTranslation();
+  const theme = getThemeColors();
 
-	const changeLanguage = (language: string) => {
-		i18n.changeLanguage(language);
-		localStorage.setItem("language", language);
-		document.body.dir = language === "ar" ? "rtl" : "ltr";
-	};
-	return (
-		<button
-			onClick={() =>
-				changeLanguage(i18n.language === "ar" ? "en" : "ar")
-			}
-			style={{
-				color: theme.primary,
-				backgroundColor: "white",
-			}}
-			className='flex items-center text-xs cursor-pointer shadow-lg gap-1 p-1 px-2 relative rounded-lg'
-		>
-			{i18n.language === "ar" ? "English" : "اللغة العربية"}
-			<Globe
-				size={16}
-				style={{ color: theme.secondary }}
-			/>
-		</button>
-	);
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+    localStorage.setItem("language", language);
+    document.body.dir = language === "ar" ? "rtl" : "ltr";
+  };
+  return (
+    <button
+      onClick={() => changeLanguage(i18n.language === "ar" ? "en" : "ar")}
+      style={{
+        color: theme.primary,
+        backgroundColor: "white",
+      }}
+      className="flex items-center text-xs cursor-pointer shadow-lg gap-1 p-1 px-2 relative rounded-lg"
+    >
+      {i18n.language === "ar" ? "اللغة العربية" : "English"}
+      <Globe size={16} style={{ color: theme.secondary }} />
+    </button>
+  );
 }
 
 export default LanguageSelector;
