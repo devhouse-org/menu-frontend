4c4969f13605aee14272ca6b8a95380af6fbe5f8
Yaser Asaad <yaser.asaad@iqdevhouse.com>
2024-09-12 11:56:24 +0300

    framer motion added

--- src/components/RatingSelector.tsx
+++ src/components/RatingSelector.tsx
@@ -1,5 +1,5 @@
-// RatingStar.tsx
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion"; // Import Framer Motion
 
 interface RatingStarProps {
   rating: number;
@@ -24,7 +24,7 @@ const RatingSelector: React.FC<RatingStarProps> = ({
   return (
     <div className="flex items-center mt-4">
       {[1, 2, 3, 4, 5].map((star) => (
-        <svg
+        <motion.svg
           key={star}
           className={`w-10 h-10 cursor-pointer ms-1 ${
             currentRating >= star ? "text-yellow-400" : "text-gray-300"
@@ -34,9 +34,11 @@ const RatingSelector: React.FC<RatingStarProps> = ({
           fill="currentColor"
           viewBox="0 0 22 20"
           onClick={() => handleRatingClick(star)}
+          whileTap={{ scale: 1.6 }} // Adds the scale animation on click
+          transition={{ type: "spring", stiffness: 300 }} // Spring effect for smooth scaling
         >
           <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-        </svg>
+        </motion.svg>
       ))}
     </div>
   );
